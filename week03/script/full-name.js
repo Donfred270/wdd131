7c34bc5e96206844fac69e1098528bb245fc2cc3
Donfred270 <donfredvallon@gmail.com>
2025-05-22 22:29:46 -0400

    week03

---
+++ week03/script/full-name.js
@@ -0,0 +1,12 @@
+
+
+let firstName = 'Antonia';
+let lastName = 'Francesca';
+
+function fullName(firstName, lastName){
+    return `${firstName} ${lastName}`; 
+}
+
+let username = fullName(firstName, lastName);
+console.log(username)
+
